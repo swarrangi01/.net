c28dd43e42ac4fe7a806399693ddd878c5d58f7f
Patil <swarpatil@deloitte.com>
2023-07-04 21:45:12 +0530

    added DBContext and 1 model

---
+++ CRUDOperationsAPI/Models/BrandDBContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace CRUDOperationsAPI.Models
+{
+    public class BrandDBContext : DbContext
+    {
+
+        public BrandDBContext(DbContextOptions<BrandDBContext> options) : base(options)
+        {
+
+
+        }
+
+        public DbSet<Brands> Brands { get; set; }
+
+    }
+}
---
+++ CRUDOperationsAPI/Models/Brands.cs
@@ -0,0 +1,11 @@
+﻿namespace CRUDOperationsAPI.Models
+{
+    public class Brands
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Category { get; set; }
+        public string? BestSeller { get; set; }
+
+    }
+}
