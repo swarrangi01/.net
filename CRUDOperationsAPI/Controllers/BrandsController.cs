23986d8414d329aeb95d0c3bf92ed555eea11fa8
Patil <swarpatil@deloitte.com>
2023-07-04 22:49:39 +0530

    http put/delete

--- CRUDOperationsAPI/Controllers/BrandsController.cs
+++ CRUDOperationsAPI/Controllers/BrandsController.cs
@@ -19,19 +19,19 @@ namespace CRUDOperationsAPI.Controllers
         }
 
         [HttpGet]
-        public async Task<ActionResult<IEnumerable<Brands>>> GetBrands() 
+        public async Task<ActionResult<IEnumerable<Brands>>> GetBrands()
         {
-            if (_dbContext.Brands == null) 
-            { 
-            return NotFound();
+            if (_dbContext.Brands == null)
+            {
+                return NotFound();
             }
 
             return await _dbContext.Brands.ToListAsync();
-        
+
         }
 
 
-        [HttpGet]
+        [HttpGet("{id}")]
         public async Task<ActionResult<Brands>>  GetBrandsById(int id)
         {
             if (_dbContext.Brands == null)
@@ -49,5 +49,64 @@ namespace CRUDOperationsAPI.Controllers
 
         }
 
+
+
+        [HttpPost]
+        public async Task<ActionResult<Brands>> AddBrand(Brands brand) 
+        {
+            _dbContext.Brands.Add(brand);
+            await _dbContext.SaveChangesAsync();
+
+            return brand;
+
+        
+        }
+        
+
+        [HttpPut]
+        public async Task<ActionResult<Brands>> UpdateDB(int id, Brands brand) {
+
+            if (id != brand.Id) {
+                return BadRequest();
+
+            }
+
+
+            try
+            {
+                _dbContext.Brands.Update(brand);
+                await _dbContext.SaveChangesAsync();
+                return brand;
+            }
+            catch (Exception ex) {
+                return BadRequest("Failed to update because " + ex.Message);
+            }
+
+        }
+
+
+
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<Brands>> DeleteRecord(int id)
+        {
+            if (_dbContext.Brands == null)
+            { 
+            return BadRequest(); 
+            }
+
+            var brand=_dbContext.Brands.Find(id);
+            if (brand == null)
+            {
+                return NotFound();
+            }
+            _dbContext.Brands.Remove(brand);
+            await _dbContext.SaveChangesAsync();
+            return Ok();
+        }
+
     }
+
+
+
+
 }
