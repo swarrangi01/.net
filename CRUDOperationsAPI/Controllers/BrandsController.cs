49144cdbbb6711fe44878ff2ed0b879647091c31
Patil <swarpatil@deloitte.com>
2023-07-04 22:11:21 +0530

    added controller

---
+++ CRUDOperationsAPI/Controllers/BrandsController.cs
@@ -0,0 +1,20 @@
+﻿using CRUDOperationsAPI.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CRUDOperationsAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BrandsController : ControllerBase
+    {
+        private readonly BrandDBContext _dbContext;
+
+
+        public BrandsController(BrandDBContext dBContext)
+        {
+            _dbContext = this.dbContext;
+
+        }
+    }
+}
