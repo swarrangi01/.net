4f5ec03cdf555620c133c58b8e00aa8f4875a467
Patil <swarpatil@deloitte.com>
2023-07-04 22:24:00 +0530

    hhtp get/post

--- CRUDOperationsAPI/Controllers/BrandsController.cs
+++ CRUDOperationsAPI/Controllers/BrandsController.cs
@@ -1,6 +1,7 @@
 ﻿using CRUDOperationsAPI.Models;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace CRUDOperationsAPI.Controllers
 {
@@ -13,8 +14,40 @@ namespace CRUDOperationsAPI.Controllers
 
         public BrandsController(BrandDBContext dBContext)
         {
-            _dbContext = this.dbContext;
+            _dbContext = dBContext;
 
         }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Brands>>> GetBrands() 
+        {
+            if (_dbContext.Brands == null) 
+            { 
+            return NotFound();
+            }
+
+            return await _dbContext.Brands.ToListAsync();
+        
+        }
+
+
+        [HttpGet]
+        public async Task<ActionResult<Brands>>  GetBrandsById(int id)
+        {
+            if (_dbContext.Brands == null)
+            {
+                return NotFound();
+            }
+
+            var brand = await _dbContext.Brands.FindAsync(id);
+            if (brand == null)
+            {
+                return NotFound();
+            }
+
+            return brand;
+
+        }
+
     }
 }
